7115be245ac627c80c4381c6de12bc83115aa90d
peddakurvasaikrishna <ysai5297@gmail.com>
2025-07-09 21:56:43 +0530

    Create sai.js

---
+++ Day9/sai.js
@@ -0,0 +1,44 @@
+let name="saikrishna";
+console.log(name)
+const pi=3.14;
+console.log(pi)
+var age=20;
+console.log(age)
+let a="sai";
+console.log(typeof a)
+let result=10+5;
+console.log(result)
+let remainder=10%3;
+console.log(remainder)
+let power=2**3;
+console.log(power)
+let count=0;
+count++;
+console.log(count)
+let x=10;
+x+=5;
+console.log(x)
+let y=10;
+x-=5;
+console.log(y)
+let z=10;
+x*=5;
+console.log(z)
+console.log(5=="5")
+console.log(5==="5")
+console.log(10>5)
+let c=true;
+let b=false;
+console.log(c&&b)
+console.log(c||b)
+console.log(!c)
+let years=18;
+let status=(age>=18)?"adult":"minor";
+console.log(status)
+//console.log("hello")
+/*console.log(status)
+console.log(status)
+console.log(status)*/
+
+
+
