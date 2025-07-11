0ce85ea4f4d4ded789b9520af4bcda15fb594956
peddakurvasaikrishna <ysai5297@gmail.com>
2025-07-11 18:17:08 +0530

    Create program.js

---
+++ Day11/program.js
@@ -0,0 +1,40 @@
+/*let a=prompt("enter a number");
+let even=function(num){
+    if(num%2==0){
+        return "even";
+    }
+    else{
+        return "odd";
+    }
+}
+console.log(even(a));
+//by using ternary
+let num=prompt("enter a number");
+let c=function(){
+let status=(num%2==0)? "even":"odd";
+console.log(status);}
+console.log(even(c));
+//factorial
+let number=prompt("enter a number");
+let b=(num)=>{
+    let result=1;
+    for(let i=2;i<=number;i++){
+        result*=i;
+    }
+        return result;
+}
+console.log(b(number));
+//ifelse
+let num=prompt("enter the number");
+let num1=function(){
+    if(num%3==0 && num%5==0)
+        return "fizzbuzz";
+    else if(num%3==0)
+        return "fizz";
+    else if(num%5==0)
+        return "buzz";
+    
+    else
+        return num;
+}
+console.log(num1(num))*/
